dd6628e7cc8451dde354b3be98038991283a412d
ailton142 <Ailton.novais142@gmail.com>
2025-06-16 13:30:03 -0300

    Add files via upload

---
+++ config-example.js
@@ -0,0 +1,3 @@
+// 📄 config.js (guarde só no seu PC)
+window.JSONBIN_API_KEY = '$2a$10$sua_chave_secreta_aqui'; // Cole sua Secret Key
+window.JSONBIN_BIN_ID = '684efe118960c979a5aa6291';       // Cole seu Bin ID
